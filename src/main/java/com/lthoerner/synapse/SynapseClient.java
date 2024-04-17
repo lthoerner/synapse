3ab9380b0f337654dcbfe63afceb31c20b99b063
lthoerner <lowellashton@gmail.com>
2024-04-17 12:31:00 -0700

    feat(dev): set up Fabric configuration and add necessary classes to launch the game

---
+++ src/main/java/com/lthoerner/synapse/SynapseClient.java
@@ -0,0 +1,10 @@
+package com.lthoerner.synapse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SynapseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
